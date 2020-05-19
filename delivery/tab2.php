499f9bec8bd9051ddc4e0b9dff628a3a6ff9aaf1
/usr/bin/vim <support@beget.ru>
2020-05-19 14:13:08 +0300

    delivery2

--- delivery/tab2.php
+++ delivery/tab2.php
@@ -1,55 +1,93 @@
-<p class='info-paragraph' style='font-size:20px;'>Тарифы доставки грузов по городу Санкт-Петербург в пределах КАД</p>
-<table class='skid' >
-	<tr >
-		<td><b>0-50 кг</b></td>
-		<td >600 руб.</td>
-	</tr>
-	<tr >
-		<td ><b>50-100 кг</b></td>
-		<td >750 руб.</td>
-	</tr>
-	<tr>
-		<td ><b>100-250 кг</b></td>
-		<td >1000 руб.</td>
-	</tr>
-	<tr>
-		<td ><b>250-500 кг</b></td>
-		<td >1 300 руб.</td>
-	</tr>
-<tr>
-		<td ><b>500-1 000 кг</b></td>
-		<td >1 850 руб.</td>
-	</tr>
-<tr>
-		<td ><b>1 000-1 500 кг</b></td>
-		<td >2 750 руб.</td>
-	</tr>
-<tr>
-		<td ><b>1 500-2 000 кг</b></td>
-		<td >3 850 руб.</td>
-	</tr>
-<tr>
-		<td ><b>2 000-2 500 кг</b></td>
-		<td >4 900 руб.</td>
-	</tr>
-	<tr>
-		<td ><b>2 500-3 000 кг</b></td>
-		<td >5 500 руб.</td>
-	</tr>
-	<tr>
-		<td ><b>3 000-5 000 кг</b></td>
-		<td >6 000 руб.</td>
-	</tr>
-	<tr>
-		<td ><b>5 000-10 000 кг</b></td>
-		<td >8 500 руб.</td>
-	</tr>
-	<tr>
-		<td ><b>10 000-20 000 кг</b></td>
-		<td >11 000 руб.</td>
-	</tr>
-
-</table>
+			<h2 class="s28-title">Тарифы доставки по городу Санкт-Петербург и ЛО</h2>
+			<table class="blue-table">
+				<thead class="blue-table__thead">
+					<tr class="blue-table__tr">
+						<th class="blue-table__th">Масса груза</th>
+						<th class="blue-table__th">Зона - 1</th>
+						<th class="blue-table__th">Зона - 2</th>
+						<th class="blue-table__th">Зона - 3</th>
+						<th class="blue-table__th">Зона - 4</th>
+						<th class="blue-table__th">Зона - 5</th>
+					</tr>
+				</thead>
+				<tbody class="blue-table__tbody">
+					<tr class="blue-table__tr">
+						<td class="blue-table__td">0 - 15 кг.</td>
+						<td class="blue-table__td">290</td>
+						<td class="blue-table__td">450</td>
+						<td class="blue-table__td">650</td>
+						<td class="blue-table__td">850</td>
+						<td class="blue-table__td">1 600</td>
+					</tr>
+					<tr class="blue-table__tr">
+						<td class="blue-table__td">15 - 30 кг.</td>
+						<td class="blue-table__td">350</td>
+						<td class="blue-table__td">500</td>
+						<td class="blue-table__td">750</td>
+						<td class="blue-table__td">950</td>
+						<td class="blue-table__td">1 700</td>
+					</tr>
+					<tr class="blue-table__tr">
+						<td class="blue-table__td">30 - 100 кг.</td>
+						<td class="blue-table__td">500</td>
+						<td class="blue-table__td">650</td>
+						<td class="blue-table__td">850</td>
+						<td class="blue-table__td">1 100</td>
+						<td class="blue-table__td">1 750</td>
+					</tr>
+					<tr class="blue-table__tr">
+						<td class="blue-table__td">100 - 200 кг.</td>
+						<td class="blue-table__td">950</td>
+						<td class="blue-table__td">1 000</td>
+						<td class="blue-table__td">1 150</td>
+						<td class="blue-table__td">1 400</td>
+						<td class="blue-table__td">2 000</td>
+					</tr>
+					<tr class="blue-table__tr">
+						<td class="blue-table__td">200 - 500 кг.</td>
+						<td class="blue-table__td">1 500</td>
+						<td class="blue-table__td">1 650</td>
+						<td class="blue-table__td">1 850</td>
+						<td class="blue-table__td">2 100</td>
+						<td class="blue-table__td">2 700</td>
+					</tr>
+					<tr class="blue-table__tr">
+						<td class="blue-table__td">500 - 1 000 кг.</td>
+						<td class="blue-table__td">2 000</td>						
+						<td class="blue-table__td">2 150</td>
+						<td class="blue-table__td">2 350</td>
+						<td class="blue-table__td">2 600</td>
+						<td class="blue-table__td">3 500</td>
+					</tr>
+<!--					
+					<tr class="blue-table__tr">
+						<td class="blue-table__td">1 000 - 3 000 кг.</td>
+						<td class="blue-table__td">2 900</td>
+						<td class="blue-table__td">3 300</td>
+						<td class="blue-table__td">3 800</td>
+						<td class="blue-table__td">4 500</td>
+						<td class="blue-table__td">5 500</td>
+					</tr>
+					<tr class="blue-table__tr">
+						<td class="blue-table__td">3 000 - 5 000 кг.</td>
+						<td class="blue-table__td">3 900</td>
+						<td class="blue-table__td">4 500</td>
+						<td class="blue-table__td">5 200</td>
+						<td class="blue-table__td">6 100</td>
+						<td class="blue-table__td">6 900</td>
+					</tr>
+                                        <tr class="blue-table__tr">
+						<td class="blue-table__td">5 000 - 10 000 кг.</td>
+						<td class="blue-table__td">5 900</td>
+						<td class="blue-table__td">6 300</td>
+						<td class="blue-table__td">6 800</td>
+						<td class="blue-table__td">7 500</td>
+						<td class="blue-table__td">8 500</td>
+					</tr>
+-->					
+				</tbody>
+			</table>
+			
 	<p class='info-paragraph' style='font-size:20px;'>За пределы КАД по Ленинградской области + 17 руб/км.</p>
 	
 	
