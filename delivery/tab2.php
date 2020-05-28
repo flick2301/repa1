5a48cf3c7769c06c36441925a801ac4bf38ecf77
/usr/bin/vim <support@beget.ru>
2020-05-28 16:29:40 +0300

    prod_change 280520_2

--- delivery/tab2.php
+++ delivery/tab2.php
@@ -17,51 +17,51 @@
 					<tr class="blue-table__tr">
 						<td class="blue-table__td">0 - 15 кг.</td>
 						<td class="blue-table__td">290</td>
-						<td class="blue-table__td">450</td>
-						<td class="blue-table__td">650</td>
-						<td class="blue-table__td">850</td>
-						<td class="blue-table__td">1 600</td>
+						<td class="blue-table__td">490</td>
+						<td class="blue-table__td">800</td>
+						<td class="blue-table__td">1 300</td>
+						<td class="blue-table__td">1 900</td>
 					</tr>
 					<tr class="blue-table__tr">
 						<td class="blue-table__td">15 - 30 кг.</td>
 						<td class="blue-table__td">350</td>
-						<td class="blue-table__td">500</td>
-						<td class="blue-table__td">750</td>
-						<td class="blue-table__td">950</td>
-						<td class="blue-table__td">1 700</td>
+						<td class="blue-table__td">590</td>
+						<td class="blue-table__td">1 000</td>
+						<td class="blue-table__td">1 600</td>
+						<td class="blue-table__td">2 300</td>
 					</tr>
 					<tr class="blue-table__tr">
 						<td class="blue-table__td">30 - 100 кг.</td>
-						<td class="blue-table__td">500</td>
 						<td class="blue-table__td">650</td>
-						<td class="blue-table__td">850</td>
-						<td class="blue-table__td">1 100</td>
-						<td class="blue-table__td">1 750</td>
-					</tr>
-					<tr class="blue-table__tr">
-						<td class="blue-table__td">100 - 200 кг.</td>
-						<td class="blue-table__td">950</td>
-						<td class="blue-table__td">1 000</td>
-						<td class="blue-table__td">1 150</td>
-						<td class="blue-table__td">1 400</td>
-						<td class="blue-table__td">2 000</td>
-					</tr>
-					<tr class="blue-table__tr">
-						<td class="blue-table__td">200 - 500 кг.</td>
-						<td class="blue-table__td">1 500</td>
-						<td class="blue-table__td">1 650</td>
-						<td class="blue-table__td">1 850</td>
-						<td class="blue-table__td">2 100</td>
+						<td class="blue-table__td">900</td>
+						<td class="blue-table__td">1 300</td>
+						<td class="blue-table__td">1 900</td>
 						<td class="blue-table__td">2 700</td>
 					</tr>
 					<tr class="blue-table__tr">
-						<td class="blue-table__td">500 - 1 000 кг.</td>
-						<td class="blue-table__td">2 000</td>						
-						<td class="blue-table__td">2 150</td>
-						<td class="blue-table__td">2 350</td>
+						<td class="blue-table__td">100 - 200 кг.</td>
+						<td class="blue-table__td">1 000</td>
+						<td class="blue-table__td">1 300</td>
+						<td class="blue-table__td">1 700</td>
+						<td class="blue-table__td">2 300</td>
+						<td class="blue-table__td">3 000</td>
+					</tr>
+					<tr class="blue-table__tr">
+						<td class="blue-table__td">200 - 500 кг.</td>
+						<td class="blue-table__td">1 950</td>
+						<td class="blue-table__td">2 250</td>
 						<td class="blue-table__td">2 600</td>
-						<td class="blue-table__td">3 500</td>
-					</tr>			
+						<td class="blue-table__td">3 250</td>
+						<td class="blue-table__td">4 000</td>
+					</tr>
+					<tr class="blue-table__tr">
+						<td class="blue-table__td">500 - 1 000 кг.</td>
+						<td class="blue-table__td">2 600</td>
+						<td class="blue-table__td">2 900</td>
+						<td class="blue-table__td">3 300</td>
+						<td class="blue-table__td">3 900</td>
+						<td class="blue-table__td">4 700</td>
+					</tr>
 					<tr class="blue-table__tr">
 						<td class="blue-table__td">1 000 - 3 000 кг.</td>
 						<td class="blue-table__td">2 900</td>
@@ -78,7 +78,7 @@
 						<td class="blue-table__td">6 100</td>
 						<td class="blue-table__td">6 900</td>
 					</tr>
-                                        <tr class="blue-table__tr">
+                    <tr class="blue-table__tr">
 						<td class="blue-table__td">5 000 - 10 000 кг.</td>
 						<td class="blue-table__td">5 900</td>
 						<td class="blue-table__td">6 300</td>
@@ -86,7 +86,6 @@
 						<td class="blue-table__td">7 500</td>
 						<td class="blue-table__td">8 500</td>
 					</tr>
-				
 				</tbody>
 			</table>
 			<br /><br />
