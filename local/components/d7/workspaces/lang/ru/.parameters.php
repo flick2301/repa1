40e58eb18d06e1817d80d8fff356e0c139923883
/usr/bin/vim <support@beget.ru>
2020-06-14 10:41:30 +0300

    140620_2

--- local/components/d7/workspaces/lang/ru/.parameters.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS["SHOPS_SETTING"] = "Дополнительно";
-$MESS["SHOPS_TYPE_IBLOCK"] = "Тип инфоблока";
-$MESS["SHOPS_ID_IBLOCK"] = "Инфоблок для отображения";
-$MESS["SHOPS_SECTION"] = "Разделы магазинов";
-?>
