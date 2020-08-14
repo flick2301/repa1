cd75b9e5c53dd9af6bd9031e4ed91fe133888a15
/usr/bin/vim <support@beget.ru>
2020-08-14 09:06:45 +0300

    change02

--- local/templates/moskrep/components/bitrix/catalog.section/horizontal_new/template.php
+++ local/templates/moskrep/components/bitrix/catalog.section/horizontal_new/template.php
@@ -186,7 +186,7 @@ if($arParams['FOR_SEO']!='Y'){
 									</div>
 							</span>
 							<span class="delivery-view"  data-product="<?=$item['ID']?>">
-									<div id='delivery_<?=$item['ID']?>' style='display:none' class="delivery-block">
+									<div id='delivery_<?=$item['ID']?>' class="delivery-block">
 										
 									</div>
 							</span>
