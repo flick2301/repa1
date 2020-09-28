77012e76b09b05065b9007fb483729286f2d61bb
/usr/bin/vim <support@beget.ru>
2020-09-28 12:52:01 +0300

    changeC

--- addresses/index.php
+++ addresses/index.php
@@ -30,7 +30,18 @@ $APPLICATION->SetTitle("Адреса магазинов");
 <?endif?>
 
 <div id='vivoz_2' class='delivery__tabs-list city2565 city2631 spb'> 
-<?require_once($_SERVER["DOCUMENT_ROOT"] . "/kontent-elementa/contact_spb.php");?>
+
+<?//require_once($_SERVER["DOCUMENT_ROOT"] . "/kontent-elementa/contact_spb.php");?>
+
+	<?$APPLICATION->IncludeComponent("d7:contact_shops","krep-komp",Array(
+				"IBLOCK_ID" => "19", 
+				"SECTION_ID" => SHOPS_SPB,
+				"CACHE_TYPE" => "A", 
+                "CACHE_TIME" => "3600", 
+                "CACHE_FILTER" => "N" 
+                    ), false
+    );?>	
+
 </div>
 
 
