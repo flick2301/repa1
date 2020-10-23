a8a85685a961b1e5f75ed8cf68d0df9b47209221
/usr/bin/vim <support@beget.ru>
2020-10-23 15:41:14 +0300

    changeC

--- include/email_header.php
+++ include/email_header.php
@@ -3,7 +3,7 @@
 <?elseif($_SERVER['HTTP_HOST']=='kazan.krep-komp.ru'):?>
 <a href='mailto:kazan@krep-komp.ru' class="header-contacts__mail" id='header_mail' >kazan@krep-komp.ru</a>
 <?elseif($_SERVER['HTTP_HOST']=='nizhniy-novgorod.krep-komp.ru'):?>
-<a href='mailto:nizhniy-novgorod@krep-komp.ru' class="header-contacts__mail" id='header_mail' >nizhniy-novgorod@krep-komp.ru</a>
+<a href='mailto:nizhniy-novgorod@krep-komp.ru' class="header-contacts__mail" id='header_mail' >nn@krep-komp.ru</a>
 <?elseif($_SERVER['HTTP_HOST']=='voronezh.krep-komp.ru'):?>
 <a href='mailto:voronezh@krep-komp.ru' class="header-contacts__mail" id='header_mail' >voronezh@krep-komp.ru</a>
 <?else:?>
