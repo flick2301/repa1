fe0c7a29df1bedd9a1740b8190f35e971497ee43
Dmitry Tregub <flicknetmail@yahoo.com>
2021-02-26 13:19:59 +0300

    change

---
+++ local/components/d7/contact_shops/templates/contacts/script.js
@@ -0,0 +1,106 @@
+function init(id, lat, lon) {
+	
+	 getRoute = false;
+	 myRoute = false;
+	
+	 return function () {
+	    window['myMap' + id] = new ymaps.Map('map-' + id, {
+            center: [lat, lon],
+			controls: [],
+            zoom: 15
+        }, {
+        });
+
+		if(window.innerWidth >= 767){
+    var pixelCenter = window['myMap' + id].getGlobalPixelCenter();
+    pixelCenter = [
+     pixelCenter[0] - 220,
+     pixelCenter[1] - 0
+    ];
+    var geoCenter = window['myMap' + id].options.get('projection').fromGlobalPixels(pixelCenter, window['myMap' + id].getZoom());
+    window['myMap' + id].setCenter(geoCenter);
+   }
+		
+        var myPlacemark = new ymaps.Placemark([lat, lon], {
+            hintContent: '',
+            balloonContent: ''
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: template_url + '/images/shop_label.svg',
+            // Размеры метки.
+            iconImageSize: [30, 30],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-15, -15]
+        });
+		
+		window['myMap' + id].geoObjects.add(myPlacemark);	
+
+		window['suggestView' + id] = new ymaps.SuggestView('form-map-from-' + id);
+		
+myCollection = new ymaps.GeoObjectCollection({}, {
+    preset: 'islands#blueIcon', //все метки красные
+    draggable: false, // и их можно перемещать
+  });		
+		
+	window['suggestView' + id].events.add("select", function(e) {
+	ymaps.geocode(e.get('item').value).then(function (res) {
+		
+		if (getRoute) {
+			//myCollection.remove(myPlacemark2); 
+			if (myRoute) window['myMap' + id].geoObjects.remove(myRoute);
+			console.log(myRoute);
+		}
+		
+		myPlacemark2 = new ymaps.Placemark(res.geoObjects.get(0).geometry.getCoordinates(), {}, {
+			preset: 'islands#circleIcon',
+            iconColor: '#4F36E3'
+			});
+			
+
+  // добавляем коллекцию на карту
+  window['myMap' + id].geoObjects.add(myCollection);				
+			
+		
+		//myCollection.add(myPlacemark2);	
+		//console.log(res.geoObjects.get(0).geometry.getCoordinates());		
+
+
+var start_point = [lat, lon];
+var end_point = res.geoObjects.get(0).geometry.getCoordinates();
+ymaps.route([start_point, end_point], {
+ mapStateAutoApply: true,
+ avoidTrafficJams: false,
+ multiRoute: false,
+ routingMode: "auto",
+ viaIndexes: []
+}).then(function (route) {
+ /*route.getPaths().options.set('strokeColor', '#552FEC');*/
+ var points = route.getWayPoints();  
+ points.get(0).properties.set('balloonContent', 'Дистанция: ' + route.getHumanLength() + '. Продолжительность: ' + route.getHumanTime() + '.');
+ points.get(1).properties.set('balloonContent', 'Дистанция: ' + route.getHumanLength() + '. Продолжительность: ' + route.getHumanTime() + '.');
+ points.get(0).properties.set('iconContent', '');
+ points.get(1).properties.set('iconContent', '');
+ points.get(0).options.set('iconColor', '#4F36E3');
+ points.get(1).options.set('iconColor', '#4F36E3');
+ points.get(0).options.set('preset', 'islands#circleIcon');
+ points.get(1).options.set('preset', 'islands#circleIcon');
+ points.get(0).options.set('visible', false);
+ points.get(1).options.set('visible', true);
+ window['myMap' + id].geoObjects.add(myRoute = route);
+}, function (error) {
+ // Ошибка error.message
+});	
+
+getRoute = true;
+		
+	});		
+
+	
+  });
+		
+	 }
+}
---
+++ local/components/d7/contact_shops/templates/contacts/template.php
@@ -0,0 +1,50 @@
+<?
+global $request;
+$scheme = $request->isHttps() ? 'https' : 'http';
+
+switch (LANGUAGE_ID)
+{
+	case 'ru':
+		$locale = 'ru-RU'; break;
+	case 'ua':
+		$locale = 'ru-UA'; break;
+	case 'tk':
+		$locale = 'tr-TR'; break;
+	default:
+		$locale = 'en-US'; break;
+}
+?>
+
+
+<?/*<script src="<?=$scheme?>://api-maps.yandex.ru/2.1.50/?load=package.full&lang=<?=$locale?>"></script>*/?>
+<?$api_key = htmlspecialcharsbx(Bitrix\Main\Config\Option::get('fileman', 'yandex_map_api_key'));?>
+<?$APPLICATION->AddHeadScript("{$scheme}://api-maps.yandex.ru/2.1.50/?load=package.full&lang={$locale}&apikey={$api_key}" );?>
+            
+			  
+<script>
+	template_url = '<?=$this->GetFolder()?>';
+</script>			  
+			  
+			  
+			  <div class="contacts__list">
+<?foreach($arResult["ITEMS"] AS $key=>$item):?>		
+                <div class="contacts__box">
+                  <div class="contacts__map">
+                    <?//=str_replace(Array(490, 370), Array("100%", "100%"), $item["DETAIL_TEXT"])?>
+					<div class="contacts__search"><textarea name="form-map-from" class="contacts__from" id="form-map-from-<?=++$id?>"></textarea></div>
+					<div id="map-<?=$id?>" class="contacts__mapframe"></div>
+                  </div>
+                  <div class="contacts__left">
+                    <div class="contacts__topic"><?=str_replace(" в ", "<br />в ", $item["NAME"])?></div>
+                    <div class="contacts__address"><?=$item["PROP"]["ADDRESS"]["~VALUE"]?></div>
+                    <div class="contacts__time"><?=strip_tags($item["PREVIEW_TEXT"])?></div>
+					<a class="contacts__email" href="mailto:<?=$item["PROP"]["EMAIL"]["VALUE"]?>"><?=$item["PROP"]["EMAIL"]["VALUE"]?></a>
+					<?if($item["PROP"]["PHONE"]["VALUE"]):?><a class="contacts__phone" href="tel:<?=str_replace(Array(" ", "-", "(", ")"), "", $item["PROP"]["PHONE"]["VALUE"])?>"><?=str_replace(array("+7", "(", ")"), array("8", "", ""), $item["PROP"]["PHONE"]["VALUE"])?></a><?endif?>
+                  </div>
+                </div>
+				
+				<?if($id && $item["PROP"]["LAT"]["VALUE"] && $item["PROP"]["LON"]["VALUE"]):?><script>ymaps.ready(init(<?=$id?>, <?=$item["PROP"]["LAT"]["VALUE"]?>, <?=$item["PROP"]["LON"]["VALUE"]?>));</script><?endif?>
+<?endforeach?>				
+              </div>
+
+
