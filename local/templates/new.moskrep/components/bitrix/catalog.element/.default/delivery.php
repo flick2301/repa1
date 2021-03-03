d18ec3b2ce647617a45ee19da655f7a128f8f324
Dmitry Tregub <flicknetmail@yahoo.com>
2021-03-03 09:57:02 +0300

    changeC

--- local/templates/new.moskrep/components/bitrix/catalog.element/.default/delivery.php
+++ local/templates/new.moskrep/components/bitrix/catalog.element/.default/delivery.php
@@ -20,12 +20,17 @@ switch (LANGUAGE_ID)
 <?$APPLICATION->AddHeadScript("{$scheme}://api-maps.yandex.ru/2.1.50/?load=package.full&lang={$locale}&apikey={$api_key}" );?>
 <?$APPLICATION->AddHeadScript("/delivery/map.js?".rand());?>
 
+<?$APPLICATION->SetAdditionalCSS($APPLICATION->GetCurDir()."style.css", true);?> <?/*<script src="<?=$scheme?>://api-maps.yandex.ru/2.1.50/?load=package.full&lang=<?=$locale?>"></script>*/?> 
+<?$api_key = htmlspecialcharsbx(Bitrix\Main\Config\Option::get('fileman', 'yandex_map_api_key'));?> 
+<?$APPLICATION->AddHeadScript("{$scheme}://api-maps.yandex.ru/2.1.50/?load=package.full&lang={$locale}&apikey={$api_key}" );?> 
+<?$APPLICATION->AddHeadScript($APPLICATION->GetCurDir()."map.js?".rand());?> <?globalGetTitle()?> <!--simple-article-->
+
 
             <div class="basic-layout__module simple-article" id="delivery">
                   <!--simple-article-->
                   <div class="basic-layout__module simple-article">
 
-	<?$APPLICATION->IncludeComponent("d7:delivery","",Array(
+	<?$APPLICATION->IncludeComponent("d7:delivery","krep-komp",Array(
 				"IBLOCK_ID" => "22", 
 				"CACHE_TYPE" => "A", 
                 "CACHE_TIME" => "3600", 
