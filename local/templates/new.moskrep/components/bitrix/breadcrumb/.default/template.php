c21275dd53770cd4149d7a39a7820f8f7c8eb4c4
Dmitry Tregub <flicknetmail@yahoo.com>
2022-08-04 11:51:15 +0300

    04082022

--- local/templates/new.moskrep/components/bitrix/breadcrumb/.default/template.php
+++ local/templates/new.moskrep/components/bitrix/breadcrumb/.default/template.php
@@ -20,71 +20,67 @@ if(empty($arResult))
 	return "";
 
 $strReturn = '';
-
+$url_of_page = $arResult[count($arResult)-1]['LINK'];
+$url_of_page_from_builder = $sec_builder->curSection['SECTION_PAGE_URL'];
 $code = $sec_builder->curSorting[0]['CODE'] ?? $arCode[0];
+
+if($url_of_page != $url_of_page_from_builder) {
 //БЛОК СОСТАВЛЕНИЯ НАВИГАЦИОННОЙ ЦЕПОЧКИ ДЛЯ ВИРТУАЛЬНОГО(НЕСУЩЕСТВУЮЩЕГО) КАТАЛОГА( ПРИСОЕДИНЕННОЙ К КАТАЛОГУ ИЗ 1С)
-$arFilter = array("IBLOCK_ID"=>SORTING_IBLOCK_ID, 'IBLOCK_SECTION_ID'=>$sec_builder->curSorting[0]['IBLOCK_SECTION_ID'], "=CODE" => $code);
-$ar_element = CIBlockElement::GetList(array('SORT' => 'asc'),$arFilter, array('*'));
+    $arFilter = array("IBLOCK_ID" => SORTING_IBLOCK_ID, 'IBLOCK_SECTION_ID' => $sec_builder->curSorting[0]['IBLOCK_SECTION_ID'], "=CODE" => $code);
+    $ar_element = CIBlockElement::GetList(array('SORT' => 'asc'), $arFilter, array('*'));
 
-if($item_element = $ar_element->GetNextElement())
-{
+    if ($item_element = $ar_element->GetNextElement()) {
 
-    if($sorting_index==0)
-	{
-		$arResult=array();
-		$link = '';
-	}else
-	{
-        array_pop($url_code_ar);
-        $link='/'.implode("/", $url_code_ar);
-            
+        if ($sorting_index == 0) {
+            $arResult = array();
+            $link = '';
+        } else {
+            array_pop($url_code_ar);
+            $link = '/' . implode("/", $url_code_ar);
+
+        }
+        $sorting_index++;
+
+
+        $arProps = $item_element->GetProperties();
+        $arFields = $item_element->GetFields();
+
+
+        $arResult_sort[] = array('TITLE' => $arProps['H1']['VALUE'], 'LINK' => $link);
+
+
+        $nav = CIBlockSection::GetNavChain(false, $arFields["IBLOCK_SECTION_ID"]);
+        while ($arNav = $nav->GetNext()) {
+
+            $res_sect = CIBlockSection::GetList(array("SORT" => "ASC"), array("IBLOCK_ID" => SORTING_IBLOCK_ID, 'ID' => $arNav['ID']), false, array('CODE', 'UF_DIRECTORY'));
+            if ($arSect = $res_sect->GetNext()) {
+
+                if ($arSect['UF_DIRECTORY']) {
+                    //ПОДКЛЮЧАЕМ ЦЕПОЧКУ КАТАЛОГА 1С
+                    $arFilter = array(
+                        "IBLOCK_TYPE" => 'catalog',
+                        "ID" => $arSect['UF_DIRECTORY']
+                    );
+                    $id_ar = CIBlockSection::GetList(array('SORT' => 'asc'), $arFilter);
+                    $id_sec = $id_ar->GetNext();
+
+                    $nav2 = CIBlockSection::GetNavChain(false, $id_sec['ID']);
+                    $link = '/';
+
+                    while ($nav_item = $nav2->Fetch()) {
+                        $link .= $nav_item['CODE'] . '/';
+                        $arResult[] = array('TITLE' => $nav_item['NAME'], 'LINK' => $link);
+                    }
+
+
+                }
+            }
+        }
     }
-    $sorting_index++;
-
-
-    $arProps = $item_element->GetProperties();
-    $arFields = $item_element->GetFields();
-
-
-    $arResult_sort[] = array('TITLE'=>$arProps['H1']['VALUE'], 'LINK' => $link);
-    
-		
-	$nav = CIBlockSection::GetNavChain(false, $arFields["IBLOCK_SECTION_ID"]);
-	while($arNav = $nav->GetNext())
-	{
-				
-		$res_sect = CIBlockSection::GetList(array("SORT"=>"ASC"), array("IBLOCK_ID"=>SORTING_IBLOCK_ID, 'ID'=>$arNav['ID']), false, Array('CODE', 'UF_DIRECTORY'));
-		if($arSect = $res_sect->GetNext())
-		{
-					
-			if($arSect['UF_DIRECTORY'])
-			{
-				//ПОДКЛЮЧАЕМ ЦЕПОЧКУ КАТАЛОГА 1С	
-				$arFilter = array(
-						"IBLOCK_TYPE" => 'catalog',
-						"ID" => $arSect['UF_DIRECTORY']
-					);
-				$id_ar = CIBlockSection::GetList(array('SORT' => 'asc'),$arFilter);
-				$id_sec = $id_ar->GetNext();
-
-				$nav2 = CIBlockSection::GetNavChain(false, $id_sec['ID']);
-				$link='/';
-
-				while($nav_item=$nav2->Fetch())
-				{
-					$link .=$nav_item['CODE'].'/';
-					$arResult[] = array('TITLE'=>$nav_item['NAME'], 'LINK' => $link);
-				}
-				
-							
-			}	
-		}
-	}
+    if (count($arResult_sort)) {
+        $arResult = array_merge($arResult, array_reverse($arResult_sort));
+    }
 }
-if(count($arResult_sort)){
-    $arResult = array_merge($arResult, array_reverse($arResult_sort));
-}
-
 //БЛОК СОСТАВЛЕНИЯ НАВИГАЦИОННОЙ ЦЕПОЧКИ ДЛЯ ВИРТУАЛЬНОГО(НЕСУЩЕСТВУЮЩЕГО) КАТАЛОГА (КОНЕЦ)
 
 
